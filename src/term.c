7d44ffe56d663a7f260cadab3d676321063d3e2d
rumpus <tschp@pm.me>
2025-10-24 23:26:04 -0400

    esc seq

--- src/term.c
+++ src/term.c
@@ -14,9 +14,20 @@ void term_uncook(Term *p_term){
 
 	cur->c_cc[VMIN] = 1;
 	cur->c_cc[VTIME] = 0;
+	
 	tcsetattr(STDIN_FILENO, TCSANOW, cur);
+
+	printf("\x1b[?25l"); // hide cursor
+	printf("\x1b[s"); // save cursor pos
+	printf("\x1b[?47h"); // save screen
+	printf("\x1b[?1049h"); // alt buff
 }
 
 void term_revert(Term *p_term) {
-	tcsetattr(STDIN_FILENO, TCSANOW, &p_term->previous);
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &p_term->previous);
+
+	printf("\x1b[?1049l"); // return buff
+	printf("\x1b[?47l"); // restore screen
+	printf("\x1b[u"); // restore cursor position
+	printf("\x1b[?25h"); // restore cursor
 }
